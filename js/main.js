57795982a5662515d7283420993505f4cbf0aae2
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-08 12:57:00 -0700

    Add .findIndex method to find the specific item of index and delete the item.

--- js/main.js
+++ js/main.js
@@ -3,42 +3,49 @@ const people = [
     { name: 'Kait', year: 1986 },
     { name: 'Irv', year: 1970 },
     { name: 'Lux', year: 2015 }
-  ];
-  const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+];
+const comments = [
+{ text: 'Love this!', id: 523423 },
+{ text: 'Super good', id: 823423 },
+{ text: 'You are the best', id: 2039842 },
+{ text: 'Ramen is my fav food ever', id: 123523 },
+{ text: 'Nice Nice Nice!', id: 542328 }
+];
 
-  // Some and Every Checks
-  // Array.prototype.some()
-  // is at least one person 19 or older?
-  const isAdult = people.some(person => {
-    const currentYear = (new Date()) .getFullYear();
-    return currentYear - person.year >= 19;
-  });
+// Some and Every Checks
+// Array.prototype.some()
+// is at least one person 19 or older?
+const isAdult = people.some(person => {
+const currentYear = (new Date()) .getFullYear();
+return currentYear - person.year >= 19;
+});
+console.log({isAdult});
 
-  console.log({isAdult});
+// Array.prototype.every()
+// is everyone 19 or older?
+const allAdult = people.every(person => {
+const currentYear = (new Date()) .getFullYear();
+return currentYear - person.year >= 19;
+});
+console.log({allAdult});
 
-  // Array.prototype.every()
-  // is everyone 19 or older?
-  const allAdult = people.every(person => {
-    const currentYear = (new Date()) .getFullYear();
-    return currentYear - person.year >= 19;
-  });
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// Find the comment with the ID of 823423
+const findComment = comments.find(comment => comment.id === 823423);
+console.log(findComment);
 
-  console.log({allAdult});
+// Array.prototype.findIndex()
+// Find the comment with this ID
+const findIndex = comments.findIndex(comment => comment.id ===  823423);
+console.log({findIndex});
 
-  // Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  // Find the comment with the ID of 823423
-  const findComment = comments.find(comment => comment.id === 823423);
+// Delete the comment with the ID of 823423 and print an array without modifying the original one
+const newComments = [
+    ...comments.slice(0, findIndex),
+    ...comments.slice(findIndex + 1)
+];
+console.table(newComments);
 
-  console.log({findComment});
-
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-
-  // Delete the comment with the ID of 823423
+// The original array is not modified
+console.table(comments);
